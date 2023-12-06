4e0fb3f533a836cf32f4d7da69058309c82b2c46
Orino1 <orinomohamed@gmail.com>
2023-12-06 22:56:42 +0100

    Adding login route

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from flask import (
     Flask,
     render_template,
     request, redirect,
-    url_for
+    url_for,
+    make_response
 )
 from authentication import authenticate
 
@@ -50,12 +51,28 @@ def dashboard():
     return 'ok'
 
 
-@app.route("/login")
-def dashboard():
+@app.route("/login", methods=['POST', 'GET'])
+def login():
     """
-    for testing
+    Handle user login.
+
+    If user is authenticated, redirect to the dashboard page.
+    If the request method is POST, attempt to log in the user.
     """
-    return 'ok'
+    if authenticate.isAuthenticated(request):
+        return redirect(url_for('profile'))
+
+    if request.method == 'POST':
+        errors = authenticate.loginUser(request)
+        if not isinstance(errors, list):
+            session = errors
+            response = make_response(redirect(url_for('profile')))
+            response.set_cookie('session', session, max_age=3600)
+            return response
+        else:
+            return render_template("login.html", errors=errors)
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
